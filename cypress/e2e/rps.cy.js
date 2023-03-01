ef0866c85533b9523c6cd0ab37cb216c3a0bf7aa
Callum Carnegie <ccarnegie98@gmail.com>
2023-03-01 10:12:30 +0000

    updating rock paper scissors tests

--- cypress/e2e/rps.cy.js
+++ cypress/e2e/rps.cy.js
@@ -1,57 +1,59 @@
 describe("template spec", () => {
+	const url = "rps.html";
+
 	it("displays user choice", () => {
-		cy.visit("http://127.0.0.1:5500/rps.html");
+		cy.visit(url);
 
 		cy.get("#rock").click();
-		cy.get("#user-option").should("exist").contains("Rock");
+		cy.get("#user-option").should("have.text", "Rock");
 
 		cy.get("#paper").click();
-		cy.get("#user-option").should("exist").contains("Paper");
+		cy.get("#user-option").should("have.text", "Paper");
 
 		cy.get("#scissors").click();
-		cy.get("#user-option").should("exist").contains("Scissors");
+		cy.get("#user-option").should("have.text", "Scissors");
 	});
 
 	it("test win conditions", () => {
-		cy.visit("http://127.0.0.1:5500/rps.html");
+		cy.visit(url);
 
 		for (var i = 0; i < 10; i++) {
-			// check rock
+			// check rock results
 			cy.get("#rock").click();
 
 			cy.get("#computer-option").then(($span) => {
 				if ($span.text() == "Rock") {
-					cy.get("#result").should("exist").contains("Draw");
+					cy.get("#result").should("have.text", "Draw");
 				} else if ($span.text() == "Paper") {
-					cy.get("#result").should("exist").contains("Lose");
+					cy.get("#result").should("have.text", "Lose");
 				} else {
-					cy.get("#result").should("exist").contains("Win");
+					cy.get("#result").should("have.text", "Win");
 				}
 			});
 
-			// check rock
+			// check paper results
 			cy.get("#paper").click();
 
 			cy.get("#computer-option").then(($span) => {
 				if ($span.text() == "Rock") {
-					cy.get("#result").should("exist").contains("Win");
+					cy.get("#result").should("have.text", "Win");
 				} else if ($span.text() == "Paper") {
-					cy.get("#result").should("exist").contains("Draw");
+					cy.get("#result").should("have.text", "Draw");
 				} else {
-					cy.get("#result").should("exist").contains("Lose");
+					cy.get("#result").should("have.text", "Lose");
 				}
 			});
 
-			// check rock
+			// check scissors results
 			cy.get("#scissors").click();
 
 			cy.get("#computer-option").then(($span) => {
 				if ($span.text() == "Rock") {
-					cy.get("#result").should("exist").contains("Lose");
+					cy.get("#result").should("have.text", "Lose");
 				} else if ($span.text() == "Paper") {
-					cy.get("#result").should("exist").contains("Win");
+					cy.get("#result").should("have.text", "Win");
 				} else {
-					cy.get("#result").should("exist").contains("Draw");
+					cy.get("#result").should("have.text", "Draw");
 				}
 			});
 		}
